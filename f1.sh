ab53c7b5d0622d5809aaea292dcb3bd05bd30a89
root <root@ip-172-31-33-132.ap-south-1.compute.internal>
2023-07-19 05:53:09 +0000

    Pushing shell scripts

---
+++ f1.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "enter your name"
+read NAME
+echo "Hi My Name is"$NAME
---
+++ f3.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+NAME="Sannu Uthappa"
+echo "u wanna see who the admin is?"
+read "Y/N"
+echo $NAME
